1408b5cb11e1842c02039cb34be6968abba3ef89
ibaronn <ibaronn@github.com>
2026-05-08 18:58:46 +0200

    Use Ruby xcodeproj to create project

--- .github/scripts/create_project.rb
+++ .github/scripts/create_project.rb
@@ -4,82 +4,80 @@ require 'xcodeproj'
 
 project_path = 'AdhkarWidget.xcodeproj'
 project = Xcodeproj::Project.new(project_path)
-project.name = 'AdhkarWidget'
 
 # --- Targets ---
+app_target = project.new_target(:application, 'AdhkarWidget', :ios, 'com.adhkar.widgetapp', deployment_target: '17.0')
+ext_target = project.new_target(:app_extension, 'AdhkarWidgetExtension', :ios, 'com.adhkar.widgetapp.extension', deployment_target: '17.0')
 
-# App target
-app_target = project.new_target(
-  :application,
-  'AdhkarWidget',
-  :ios,
-  'com.adhkar.widgetapp',
-  deployment_target: '17.0'
-)
+# --- Groups ---
+group = project.main_group
+src = group.new_group('AdhkarWidget', 'AdhkarWidget')
+models = src.new_group('Models', 'Models')
+widget = src.new_group('Widget', 'Widget')
+ext_grp = group.new_group('AdhkarWidgetExtension', 'AdhkarWidgetExtension')
 
-# Widget Extension target
-ext_target = project.new_target(
-  :app_extension,
-  'AdhkarWidgetExtension',
-  :ios,
-  'com.adhkar.widgetapp.extension',
-  deployment_target: '17.0'
-)
-
-# --- Source Files ---
-
-def add_sources(project, target, dir, file_list)
-  Dir.glob(file_list).each do |f|
-    file = project.new_file(f)
-    target.source_build_phase.add_file_reference(file)
-  end
+# Helper to add file to source phase
+def add_source(target, phase, file_ref)
+  build_file = phase.add_file_reference(file_ref)
+  build_file
 end
 
-# Add main app sources (app target)
-add_sources(project, app_target, 'AdhkarWidget', 'AdhkarWidget/AdhkarWidgetApp.swift')
-add_sources(project, app_target, 'AdhkarWidget', 'AdhkarWidget/ContentView.swift')
-add_sources(project, app_target, 'AdhkarWidget', 'AdhkarWidget/Models/*.swift')
+# --- App target files ---
+app_source_files = [
+  src.new_file('AdhkarWidget/AdhkarWidgetApp.swift'),
+  src.new_file('AdhkarWidget/ContentView.swift'),
+  models.new_file('AdhkarWidget/Models/Adhkar.swift'),
+  models.new_file('AdhkarWidget/Models/DataStore.swift'),
+  models.new_file('AdhkarWidget/Models/Views.swift'),
+]
+app_source_files.each { |f| add_source(app_target, app_target.source_build_phase, f) }
 
-# Widget files for extension target
-add_sources(project, ext_target, 'AdhkarWidget', 'AdhkarWidget/Widget/AdhkarWidget.swift')
-add_sources(project, ext_target, 'AdhkarWidget', 'AdhkarWidget/Widget/AdhkarWidgetBundle.swift')
-add_sources(project, ext_target, 'AdhkarWidget', 'AdhkarWidget/Models/*.swift')
-add_sources(project, ext_target, 'AdhkarWidget', 'AdhkarWidget/Widget/AdhkarWidgetExtension/Info.plist')
+# Info.plist for app
+app_plist = src.new_file('AdhkarWidget/Info.plist')
+app_target.resources_build_phase.add_file_reference(app_plist)
 
-# --- Info.plists ---
+# --- Extension target files ---
+ext_source_files = [
+  widget.new_file('AdhkarWidget/Widget/AdhkarWidget.swift'),
+  widget.new_file('AdhkarWidget/Widget/AdhkarWidgetBundle.swift'),
+  models.new_file('AdhkarWidget/Models/Adhkar.swift'),
+  models.new_file('AdhkarWidget/Models/DataStore.swift'),
+  models.new_file('AdhkarWidget/Models/Views.swift'),
+]
+ext_source_files.each { |f| add_source(ext_target, ext_target.source_build_phase, f) }
+
+# Info.plist for extension
+ext_plist = ext_grp.new_file('AdhkarWidgetExtension/Info.plist')
+ext_target.resources_build_phase.add_file_reference(ext_plist)
+
+# --- Build settings ---
 app_target.build_configurations.each do |config|
   config.build_settings['INFOPLIST_FILE'] = 'AdhkarWidget/Info.plist'
   config.build_settings['IPHONEOS_DEPLOYMENT_TARGET'] = '17.0'
-  config.build_settings['SWIFT_VERSION'] = '5.9'
-  config.build_settings['DEVELOPMENT_TEAM'] = ''
+  config.build_settings['SWIFT_VERSION'] = '5.0'
+  config.build_settings['PRODUCT_NAME'] = '$(TARGET_NAME)'
   config.build_settings['CODE_SIGN_STYLE'] = 'Manual'
-  config.build_settings['CODE_SIGN_IDENTITY'] = ''
-  config.build_settings['PRODUCT_NAME'] = 'AdhkarWidget'
+  config.build_settings['CODE_SIGN_IDENTITY'] = '-'
+  config.build_settings['PRODUCT_BUNDLE_IDENTIFIER'] = 'com.adhkar.widgetapp'
 end
 
 ext_target.build_configurations.each do |config|
   config.build_settings['INFOPLIST_FILE'] = 'AdhkarWidgetExtension/Info.plist'
   config.build_settings['IPHONEOS_DEPLOYMENT_TARGET'] = '17.0'
-  config.build_settings['SWIFT_VERSION'] = '5.9'
-  config.build_settings['DEVELOPMENT_TEAM'] = ''
+  config.build_settings['SWIFT_VERSION'] = '5.0'
+  config.build_settings['PRODUCT_NAME'] = '$(TARGET_NAME)'
   config.build_settings['CODE_SIGN_STYLE'] = 'Manual'
-  config.build_settings['CODE_SIGN_IDENTITY'] = ''
-  config.build_settings['PRODUCT_NAME'] = 'AdhkarWidgetExtension'
+  config.build_settings['CODE_SIGN_IDENTITY'] = '-'
+  config.build_settings['PRODUCT_BUNDLE_IDENTIFIER'] = 'com.adhkar.widgetapp.extension'
 end
 
-# --- Dependencies (app depends on extension) ---
-app_target.build_configurations.each do |config|
-  config.build_settings['TARGETED_DEVICE_FAMILY'] = '1,2'
-end
-
-# Wire up the extension as a dependency
+# --- Dependency ---
 app_target.dependencies.new(ext_target)
 
-# Embed the extension
-embed_phase = app_target.new_copy_files_build_phase('Embed App Extensions')
-embed_phase.symbol_dst_subfolder_spec = :frameworks
-ext_target.product_reference.name = 'AdhkarWidgetExtension.appex'
-embed_phase.add_file_reference(ext_target.product_reference)
+# --- Embed extension ---
+embed = app_target.new_copy_files_build_phase('Embed App Extensions')
+embed.symbol_dst_subfolder_spec = :plug_ins
+embed.add_file_reference(ext_target.product_reference)
 
 project.save
-puts "Project created successfully at #{project_path}"
+puts "Project created: #{project_path}"
